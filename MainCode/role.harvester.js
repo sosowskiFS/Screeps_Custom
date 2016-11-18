9f3db43b223f93f43cffb301be44d5e3e0c7f661
Steven Osowski <sosowski@comcast.net>
2016-11-17 20:01:55 -0500

    Makes harvesters search for more sources if blocked.

--- MainCode/role.harvester.js
+++ MainCode/role.harvester.js
@@ -4,11 +4,27 @@ var roleHarvester = {
     run: function(creep) {
         if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+            if(creep.memory.targetSource == undefined){
+                //Set a default source if none is set
+                creep.memory.targetSource = sources[0];
+                creep.memory.targetSourcePos = 0;
+            }
+
+            if(creep.harvest(creep.memory.targetSource) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.memory.targetSource);
+            } 
+            else if(creep.harvest(creep.memory.targetSource) == ERR_NO_PATH) {
+                //Change source
+                creep.memory.targetSourcePos = creep.memory.targetSourcePos + 1;
+                if (creep.memory.targetSourcePos > (sources.length - 1)){
+                    creep.memory.targetSourcePos = 0;
+                }
+                creep.memory.targetSource = sources[creep.memory.targetSourcePos];
             }
         }
         else {
+            creep.memory.targetSource = undefined;
+            creep.memory.targetSourcePos = 0;
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION ||
