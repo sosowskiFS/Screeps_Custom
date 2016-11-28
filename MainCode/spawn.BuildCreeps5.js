5a68b468103504d689eab9e5d00f0939e2ccd4c4
Montyblancs <montyblancs99@gmail.com>
2016-11-28 13:21:16 -0500

    If miners change jobs, drop resources.

--- MainCode/spawn.BuildCreeps5.js
+++ MainCode/spawn.BuildCreeps5.js
@@ -34,6 +34,7 @@ var spawn_BuildCreeps5 = {
 
 		if (storageMiners.length == 0 && upgradeMiners.length > 0) {
 			//reassign upgrade miner
+			upgradeMiners[0].drop(RESOURCE_ENERGY);
 			upgradeMiners[0].memory.jobSpecific = 'storageMiner';
 			upgradeMiners[0].memory.linkSource = strStorage[0];
 			upgradeMiners[0].memory.mineSource = strSources[0];
