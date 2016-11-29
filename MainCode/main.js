7635fa6b82588450105e181421aa7a3ad9bdd6d5
Montblanc <montyblancs99@gmail.com>
2016-11-28 20:10:37 -0500

    Attempt to make special accessable in console

--- MainCode/main.js
+++ MainCode/main.js
@@ -1,6 +1,7 @@
 //Instructions:
 //specialInstruction('claim', 'RoomName');
 //specialInstruction('vandalize', [ArrayOfRooms], 'message');
+var specialInst = require('special');
 
 //Creeps
 var creep_work = require('creep.work');
@@ -160,23 +161,6 @@ module.exports.loop = function() {
                 }
             }
         }
-    }
-}
-
-function specialInstruction(instruction, param1, param2 = '') {
-    //Special calls, call this function from the console.
-    switch (instruction) {
-        case 'claim':
-            Memory.Instruction = 'claim';
-            Memory.InstructionOps = param1;
-            console.log('Claim instruction set, target: ' & param1);
-            break;
-        case 'vandalize':
-            Memory.Instruction = 'vandalize';
-            Memory.InstructionOps = param1;
-            Memory.InstructionOps2 = param2;
-            console.log('Vandalize instruction set, targets: ' & param1 & ', message: ' & param2);
-            break;
     }
 }
 
---
+++ MainCode/special.js
@@ -0,0 +1,24 @@
+var special = {
+    function specialInstruction(instruction, param1, param2 = '') {
+        //Special calls, call this function from the console.
+        switch (instruction) {
+            case 'claim':
+                Memory.Instruction = 'claim';
+                Memory.InstructionOps = param1;
+                console.log('Claim instruction set, target: ' & param1);
+                break;
+            case 'vandalize':
+                Memory.Instruction = 'vandalize';
+                Memory.InstructionOps = param1;
+                Memory.InstructionOps2 = param2;
+                console.log('Vandalize instruction set, targets: ' & param1 & ', message: ' & param2);
+                break;
+        }
+    }
+
+    function doTest(){
+        console.log('Success');
+    }
+};
+
+module.exports = special;
