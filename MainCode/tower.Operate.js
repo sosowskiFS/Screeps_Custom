582a3d29ed32dd482615688fa86eb70b0c924b75
Montyblancs <montyblancs99@gmail.com>
2016-12-07 12:55:57 -0500

    Removes redundant tower find

--- MainCode/tower.Operate.js
+++ MainCode/tower.Operate.js
@@ -5,27 +5,16 @@ var tower_Operate = {
         var UnderAttackPos = Memory.roomsUnderAttack.indexOf(thisRoom.name);
         if (UnderAttackPos >= 0 && thisTower.energy > 0) {
             var closestHostile = thisTower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-            if (closestHostile){
+            if (closestHostile) {
                 thisTower.attack(closestHostile);
             }
         } else if (thisTower.energy > (thisTower.energyCapacity * 0.5)) {
             //Save 50% of the tower's energy to use on repelling attackers
-            //Only maintain roads
-            var closestDamagedStructure = thisTower.pos.findInRange(FIND_STRUCTURES, repairRange, {
-                filter: (structure) => (structure.structureType == STRUCTURE_RAMPART) && (structure.hitsMax - structure.hits >= 200 && structure.hits <= 25000)
+            var closestDamagedCreep = thisTower.pos.findClosestByRange(FIND_MY_CREEPS, {
+                filter: (creep) => creep.hits < creep.hitsMax
             });
-            if (closestDamagedStructure.length > 0) {
-                //Sort so lowest hitpoints is on top
-                closestDamagedStructure.sort(towerDamageCompare);
-                thisTower.repair(closestDamagedStructure[0]);
-            } else {
-                //Renable this later if it becomes needed
-                var closestDamagedCreep = thisTower.pos.findClosestByRange(FIND_MY_CREEPS, {
-                    filter: (creep) => creep.hits < creep.hitsMax
-                });
-                if (closestDamagedCreep) {
-                    thisTower.heal(closestDamagedCreep);
-                }
+            if (closestDamagedCreep) {
+                thisTower.heal(closestDamagedCreep);
             }
         }
 
