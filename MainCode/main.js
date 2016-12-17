faaa45d10ff64e2d745d6979ee1c83c37882b195
Montblanc <montyblancs99@gmail.com>
2016-12-17 14:20:38 -0500

    Temp change slipped through the cracks

--- MainCode/main.js
+++ MainCode/main.js
@@ -109,7 +109,7 @@ module.exports.loop = function() {
 
                 //Check for hostiles in this room
                 var hostiles = thisRoom.find(FIND_HOSTILE_CREEPS, {
-                    filter: (creep) => creep.getActiveBodyparts(WORK) > 0 || creep.getActiveBodyparts(CARRY) > 0 || creep.getActiveBodyparts(ATTACK) > 0 || creep.getActiveBodyparts(RANGED_ATTACK) > 0 || creep.getActiveBodyparts(HEAL) > 0
+                    filter: (creep) => (creep.getActiveBodyparts(WORK) > 0 || creep.getActiveBodyparts(CARRY) > 0 || creep.getActiveBodyparts(ATTACK) > 0 || creep.getActiveBodyparts(RANGED_ATTACK) > 0 || creep.getActiveBodyparts(HEAL) > 0) || (creep.hits <= 500)
                 });
                 if (hostiles.length > 0 && Memory.roomsUnderAttack.indexOf(thisRoom.name) === -1) {
                     Memory.roomsUnderAttack.push(thisRoom.name);
