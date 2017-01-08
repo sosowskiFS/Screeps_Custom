85d1a0bed2c988bad50cba1b0f39ab5ee5980dde
Montblanc <montyblancs99@gmail.com>
2017-01-08 09:37:16 -0500

    Mules prioritize towers first?

--- MainCode/creep.work5.js
+++ MainCode/creep.work5.js
@@ -169,8 +169,7 @@ var creep_work5 = {
                 if (!creep.memory.structureTarget) {
                     var targets = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                         filter: (structure) => {
-                            return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
+                            return (structure.structureType == STRUCTURE_TOWER) && (structure.energy < structure.energyCapacity);
                         }
                     });
                     if (targets) {
@@ -183,7 +182,8 @@ var creep_work5 = {
                     } else {
                         targets3 = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                             filter: (structure) => {
-                                return (structure.structureType == STRUCTURE_TOWER) && (structure.energy < structure.energyCapacity);
+                                return (structure.structureType == STRUCTURE_EXTENSION ||
+                                structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;;
                             }
                         });
                         if (targets3) {
