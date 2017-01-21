38208daaa35b34b01b9a71584f77c5d9fc354d2e
Montblanc <montyblancs99@gmail.com>
2017-01-21 09:40:24 -0500

    bugfix 7

--- MainCode/creep.farMining.js
+++ MainCode/creep.farMining.js
@@ -77,7 +77,11 @@ var creep_farMining = {
 					//Get the source ID while in the room
 					var markedSources = [];
 					if (Game.flags[creep.memory.homeRoom + "FarMining"]) {
-						markedSources = Game.flags[creep.memory.homeRoom + "FarMining"].pos.lookFor(LOOK_SOURCES);
+						if (Game.flags[creep.memory.homeRoom + "FarMining"].pos.roomName == creep.pos.roomName) {
+							markedSources = Game.flags[creep.memory.homeRoom + "FarMining"].pos.lookFor(LOOK_SOURCES);
+						} else if (Game.flags[creep.memory.homeRoom + "FarMining2"]) {
+							markedSources = Game.flags[creep.memory.homeRoom + "FarMining2"].pos.lookFor(LOOK_SOURCES);
+						}					
 					}
 					if (markedSources.length) {
 						creep.memory.mineSource = markedSources[0].id;
