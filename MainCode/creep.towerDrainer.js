1529f4108f6ad6c53ee99cca0c93e612b0fac553
Montblanc <montyblancs99@gmail.com>
2017-02-16 07:38:47 -0500

    GET OUTTA HERE DEADFEED

--- MainCode/creep.towerDrainer.js
+++ MainCode/creep.towerDrainer.js
@@ -2,6 +2,9 @@ var creep_towerDrainer = {
 	run: function(creep) {
 		//Should just bounce around on the border healing itself.
 		creep.heal(creep);
+		if (!creep.memory.lastHP){
+			creep.memory.lastHP = creep.hits;
+		}
 
 		if (creep.room.name != creep.memory.destination) {
 			creep.moveTo(new RoomPosition(25, 25, creep.memory.destination));
@@ -10,8 +13,11 @@ var creep_towerDrainer = {
 				creep.moveTo(creep.room.controller);
 			} else {
 				//Drawing fire
-				creep.moveTo(new RoomPosition(25, 25, creep.memory.homeRoom));
+				if(creep.memory.lastHP > creep.hits) {
+					creep.moveTo(new RoomPosition(25, 25, creep.memory.homeRoom));
+				}				
 			}
+			creep.memory.lastHP = creep.hits;
 		}
 	}
 };
