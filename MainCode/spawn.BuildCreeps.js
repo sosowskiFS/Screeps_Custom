c26e5f528df584ad9e2ce7e75536135fbadb08da
Montyblancs <montyblancs99@gmail.com>
2017-02-17 13:25:10 -0500

    Get meowtta here

--- MainCode/spawn.BuildCreeps.js
+++ MainCode/spawn.BuildCreeps.js
@@ -26,100 +26,83 @@ var spawn_BuildCreeps = {
 				priority: 'harvester',
 				sourceLocation: strSources[1]
 			});
-		} else if (Memory.roomsUnderAttack.indexOf(thisRoom.name) != -1 && salvagers.length == 0 && thisRoom.energyAvailable >= 550) {
-			if (Memory.roomsPrepSalvager.indexOf(thisRoom.name) != -1) {
-				if (thisRoom.energyAvailable >= 550 && salvagers.length == 0) {
-					var blockedRole = '';
-					if (Memory.creepInQue.indexOf(thisRoom.name) >= 0) {
-						var RoomPointer = Memory.creepInQue.indexOf(thisRoom.name)
-						blockedRole = Memory.creepInQue[RoomPointer + 1];
-					}
-					if (blockedRole != 'salvager' && strStorage.length) { //Produce a salvager unit to pick up the dropped resources
-						spawn.createCreep([MOVE, MOVE, CARRY, CARRY, CARRY, CARRY], undefined, {
-							priority: 'salvager',
-							storageTarget: strStorage[0]
-						});
-						Memory.creepInQue.push(thisRoom.name, 'salvager', '', spawn.name);
-					}
-				}
-			} else if (thisRoom.energyAvailable >= 530) {
-				//Try to produce millitary units
+		} else if (Memory.roomsUnderAttack.indexOf(thisRoom.name) != -1 && thisRoom.energyAvailable >= 850) {
+			//Try to produce millitary units
 
-				//Melee unit set: TOUGH, TOUGH, MOVE, MOVE, MOVE, ATTACK - 250
-				//Ranged unit set: MOVE, MOVE, RANGED_ATTACK - 250
+			//Melee unit set: TOUGH, TOUGH, MOVE, MOVE, MOVE, ATTACK - 250
+			//Ranged unit set: MOVE, MOVE, RANGED_ATTACK - 250
 
-				//Damaged modules do not work, put padding first.
+			//Damaged modules do not work, put padding first.
 
-				//var defenderUnits = _.filter(RoomCreeps, (creep) => creep.memory.priority == 'defender');
+			//var defenderUnits = _.filter(RoomCreeps, (creep) => creep.memory.priority == 'defender');
 
-				//var ChosenPriority = '';
-				//if (meleeUnits <= rangedUnits) {
-				//ChosenPriority = 'melee';
-				//} else {
-				//ChosenPriority = 'ranged';
+			//var ChosenPriority = '';
+			//if (meleeUnits <= rangedUnits) {
+			//ChosenPriority = 'melee';
+			//} else {
+			//ChosenPriority = 'ranged';
+			//}
+
+			var ToughCount = 0;
+			var MoveCount = 0;
+			var AttackCount = 0;
+			var RangedCount = 0;
+			var totalParts = 0;
+
+			var remainingEnergy = thisRoom.energyAvailable;
+			while ((remainingEnergy / 530) >= 1) {
+				//switch (ChosenPriority) {
+				//case 'melee':
+				//ToughCount = ToughCount + 1;
+				MoveCount = MoveCount + 3;
+				AttackCount = AttackCount + 1;
+				RangedCount = RangedCount + 2;
+				totalParts = totalParts + 6;
+				remainingEnergy = remainingEnergy - 530;
+				//break;
+				//case 'ranged':
+				//MoveCount = MoveCount + 2;
+				//RangedCount = RangedCount + 2;
+				//totalParts = totalParts + 4;
+				//remainingEnergy = remainingEnergy - 400;
+				//break;
 				//}
 
-				var ToughCount = 0;
-				var MoveCount = 0;
-				var AttackCount = 0;
-				var RangedCount = 0;
-				var totalParts = 0;
-
-				var remainingEnergy = thisRoom.energyAvailable;
-				while ((remainingEnergy / 530) >= 1) {
-					//switch (ChosenPriority) {
-					//case 'melee':
-					//ToughCount = ToughCount + 1;
-					MoveCount = MoveCount + 3;
-					AttackCount = AttackCount + 1;
-					RangedCount = RangedCount + 2;
-					totalParts = totalParts + 6;
-					remainingEnergy = remainingEnergy - 530;
-					//break;
-					//case 'ranged':
-					//MoveCount = MoveCount + 2;
-					//RangedCount = RangedCount + 2;
-					//totalParts = totalParts + 4;
-					//remainingEnergy = remainingEnergy - 400;
-					//break;
-					//}
-
-					if (totalParts >= 50) {
-						break;
-					}
+				if (totalParts >= 50) {
+					break;
 				}
-
-				var ChosenCreepSet = [];
-				while (ToughCount > 0) {
-					ChosenCreepSet.push(TOUGH);
-					ToughCount--;
-				}
-				while (MoveCount > 1) {
-					ChosenCreepSet.push(MOVE);
-					MoveCount--;
-				}
-				while (AttackCount > 0) {
-					ChosenCreepSet.push(ATTACK);
-					AttackCount--;
-				}
-				while (RangedCount > 0) {
-					ChosenCreepSet.push(RANGED_ATTACK);
-					RangedCount--;
-				}
-
-				//Insert one move module last so the creep can still run
-				ChosenCreepSet.push(MOVE);
-
-				if (ChosenCreepSet.length > 50) {
-					while (ChosenCreepSet.length > 50) {
-						ChosenCreepSet.splice(0, 1)
-					}
-				}
-
-				spawn.createCreep(ChosenCreepSet, undefined, {
-					priority: 'defender'
-				});
 			}
+
+			var ChosenCreepSet = [];
+			while (ToughCount > 0) {
+				ChosenCreepSet.push(TOUGH);
+				ToughCount--;
+			}
+			while (MoveCount > 1) {
+				ChosenCreepSet.push(MOVE);
+				MoveCount--;
+			}
+			while (AttackCount > 0) {
+				ChosenCreepSet.push(ATTACK);
+				AttackCount--;
+			}
+			while (RangedCount > 0) {
+				ChosenCreepSet.push(RANGED_ATTACK);
+				RangedCount--;
+			}
+
+			//Insert one move module last so the creep can still run
+			ChosenCreepSet.push(MOVE);
+
+			if (ChosenCreepSet.length > 50) {
+				while (ChosenCreepSet.length > 50) {
+					ChosenCreepSet.splice(0, 1)
+				}
+			}
+
+			spawn.createCreep(ChosenCreepSet, undefined, {
+				priority: 'defender'
+			});
 
 		} else if ((harvesters.length < harvesterMax || builders.length < builderMax || upgraders.length < upgraderMax) && spawn.canCreateCreep(bestWorker) == OK) {
 			var prioritizedRole = 'harvester';
