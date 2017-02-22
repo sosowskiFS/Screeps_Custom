dbf58d3b26cd220287d7016573f3ca9f8f231826
Montyblancs <montyblancs99@gmail.com>
2017-02-22 12:56:40 -0500

    The flag system was dumb.

--- MainCode/creep.combat.js
+++ MainCode/creep.combat.js
@@ -10,44 +10,19 @@ var creep_combat = {
 		}
 
 		if (Memory.roomsUnderAttack.indexOf(creep.room.name) != -1) {
-			//Move towards rampart
-			var flagName = creep.room.name + 'Rampart';
-			var flagCounter = 1;
-			var GotIt = false;
-			while (Game.flags[flagName + flagCounter.toString()]) {
-				if (Game.flags[flagName + flagCounter.toString()].pos.x == creep.pos.x && Game.flags[flagName + flagCounter.toString()].pos.y == creep.pos.y) {
-					GotIt = true;
-					break;
-				} else if (Game.flags[flagName + flagCounter.toString()].pos.lookFor(LOOK_CREEPS).length == 0) {
-					creep.moveTo(Game.flags[flagName + flagCounter.toString()]);
-					GotIt = true;
-					break;
-				} else {
-					flagCounter++;
-				}
-			}
+			//Move towards Foe, stop at rampart
 
 			var Foe = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 			if (Foe) {
-				creep.say('REEEEEEEEE', true);
 				creep.rangedAttack(Foe);
 				creep.attack(Foe);
-				if (!GotIt) {
-					creep.moveTo(Foe);
-				}
-			}
-		} else {
-			//Not under attack, move to red flags marking ramparts
-			var flagName = creep.room.name + 'Rampart';
-			var flagCounter = 1;
-			while (Game.flags[flagName + flagCounter.toString()]) {
-				if (Game.flags[flagName + flagCounter.toString()].pos.x == creep.pos.x && Game.flags[flagName + flagCounter.toString()].pos.y == creep.pos.y) {
-					break;
-				} else if (Game.flags[flagName + flagCounter.toString()].pos.lookFor(LOOK_CREEPS).length == 0) {
-					creep.moveTo(Game.flags[flagName + flagCounter.toString()]);
-					break;
+				var lookResult = creep.pos.lookFor(LOOK_STRUCTURES);
+				if (lookResult.length) {
+					if (lookResult[0].structureType != STRUCTURE_RAMPART) {
+						creep.moveTo(Foe);
+					}
 				} else {
-					flagCounter++;
+					creep.moveTo(Foe);
 				}
 			}
 		}
