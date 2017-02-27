49de7476288305eb7c761f98a4923b6d745d4c73
Montyblancs <montyblancs99@gmail.com>
2017-02-27 10:23:16 -0500

    Builder Lockup

--- MainCode/creep.work.js
+++ MainCode/creep.work.js
@@ -74,7 +74,7 @@ var creep_work = {
 					creep.moveTo(savedTarget, {
 						reusePath: moveRecalc
 					});
-				} else {
+				} else if (creep.build(savedTarget) != OK) {
 					creep.memory.structureTarget = undefined;
 				}
 			} else {
