65c1509f0e0a62448ab87a044d461d60439375e1
Montblanc <montyblancs99@gmail.com>
2017-03-04 10:41:11 -0500

    eat that error mmmmmmmmm

--- MainCode/creep.removeKebab.js
+++ MainCode/creep.removeKebab.js
@@ -11,23 +11,25 @@ var creep_Kebab = {
             while (Game.flags[flagName + flagCounter.toString()]) {
                 if (Game.flags[flagName + flagCounter.toString()].pos.lookFor(LOOK_STRUCTURES).length == 0) {
                     //Wall removed, proceed to target
-                    var sitesOnTile = Game.flags["RemoveKebab"].pos.lookFor(LOOK_STRUCTURES);
-                    if (sitesOnTile.length) {
-                        if(creep.dismantle(sitesOnTile[0]) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(sitesOnTile[0]);
+                    if (Game.flags["Removekebab"]) {
+                        var sitesOnTile = Game.flags["RemoveKebab"].pos.lookFor(LOOK_STRUCTURES);
+                        if (sitesOnTile.length) {
+                            if (creep.dismantle(sitesOnTile[0]) == ERR_NOT_IN_RANGE) {
+                                creep.moveTo(sitesOnTile[0]);
+                            }
+                            break;
+                        } else {
+                            Game.flags["RemoveKebab"].remove();
+                            //Assume target is destroyed, vandalize.
+                            var signResult = creep.signController(creep.room.controller, 'https://www.youtube.com/watch?v=HgD8-Ree07I');
+                            if (signResult == ERR_NOT_IN_RANGE) {
+                                creep.moveTo(creep.room.controller);
+                            } else if (signResult == OK) {
+                                //KEBAB REMOVED
+                                creep.suicide();
+                            }
+                            break;
                         }
-                        break;
-                    } else {
-                        Game.flags["RemoveKebab"].remove();
-                        //Assume target is destroyed, vandalize.
-                        var signResult = creep.signController(creep.room.controller,'https://www.youtube.com/watch?v=HgD8-Ree07I');
-                        if (signResult == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(creep.room.controller);
-                        } else if (signResult == OK) {
-                            //KEBAB REMOVED
-                            creep.suicide();
-                        }
-                        break;
                     }
                 } else if (creep.pos.isNearTo(Game.flags[flagName + flagCounter.toString()].pos) && Game.flags[flagName + flagCounter.toString()].pos.lookFor(LOOK_STRUCTURES).length > 0) {
                     var thisWall = Game.flags[flagName + flagCounter.toString()].pos.lookFor(LOOK_STRUCTURES);
