9820ddf37cb50b9a3e7b4a234e00c94a9f0f03c5
Montblanc <montyblancs99@gmail.com>
2017-03-18 10:47:27 -0400

    premature

--- MainCode/main.js
+++ MainCode/main.js
@@ -200,7 +200,7 @@ module.exports.loop = function() {
                 Memory.roomsUnderAttack.push(thisRoom.name);
                 if (hostiles[0].owner.username == 'Invader') {
                     Memory.roomsPrepSalvager.push(thisRoom.name);
-                } else if (Memory.RoomsAt5.indexOf(thisRoom.name) == -1) {
+                } else if (Memory.RoomsAt5.indexOf(thisRoom.name) == -1 && hostiles[0].hits > 100) {
                     //No good combat code! SAFE MODE!
                     if (!thisRoom.controller.safeMode) {
                         thisRoom.controller.activateSafeMode();
