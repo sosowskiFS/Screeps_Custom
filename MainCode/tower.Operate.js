7808d35567035062695e13f2036d6ddd5d11a71f
Montyblancs <montyblancs99@gmail.com>
2017-03-20 11:00:32 -0400

    hasFired now affects entire room

--- MainCode/tower.Operate.js
+++ MainCode/tower.Operate.js
@@ -21,14 +21,14 @@ var tower_Operate = {
 			} else if (attackDuration >= 10) {
 				maxRange = 40;
 			}
-			if (Memory.hasFired.indexOf(tower) > -1) {
+			if (Memory.hasFired.indexOf(thisRoom.name) > -1) {
 				if (maxRange <= 15) {
 					maxRange = maxRange + 10;
 				} else {
 					maxRange = maxRange + 5;
 				}
 
-				var towerPos = Memory.hasFired.indexOf(tower);
+				var towerPos = Memory.hasFired.indexOf(thisRoom.name);
 				if (towerPos >= 0) {
 					Memory.hasFired.splice(towerPos, 1);
 				}
@@ -42,7 +42,7 @@ var tower_Operate = {
 					//Probably a healer
 					if (closestHostile[1] && (closestHostile[1].getActiveBodyparts(HEAL) < 2 || thisTower.pos.getRangeTo(closestHostile[1]) < 15)) {
 						thisTower.attack(closestHostile[1]);
-						Memory.hasFired.push(tower);
+						Memory.hasFired.push(thisRoom.name);
 					} else if (thisTower.energy > (thisTower.energyCapacity * 0.5)) {
 						var closestDamagedCreep = thisTower.pos.findClosestByRange(FIND_MY_CREEPS, {
 							filter: (creep) => creep.hits < creep.hitsMax - 50
@@ -53,7 +53,7 @@ var tower_Operate = {
 					}
 				} else {
 					thisTower.attack(closestHostile[0]);
-					Memory.hasFired.push(tower);
+					Memory.hasFired.push(thisRoom.name);
 				}
 			} else if (thisTower.energy > (thisTower.energyCapacity * 0.5)) {
 				//Save 50% of the tower's energy to use on repelling attackers
