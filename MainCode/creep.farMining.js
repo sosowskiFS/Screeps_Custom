718d346ffcb93c83a7091a672ee5332c385e035e
Montblanc <montyblancs99@gmail.com>
2017-03-26 14:44:17 -0400

    I'm stupid.

--- MainCode/creep.farMining.js
+++ MainCode/creep.farMining.js
@@ -506,15 +506,79 @@ function evadeAttacker(creep) {
 		filter: (eCreep) => ((eCreep.getActiveBodyparts(ATTACK) > 0 || eCreep.getActiveBodyparts(RANGED_ATTACK) > 0) && !Memory.whiteList.includes(eCreep.owner.username))
 	});
 
-	if (Foe.length || creep.memory.lastHP > creep.hits) {
-		var spawnTarget = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-			filter: (structure) => {
-				return structure.structureType == STRUCTURE_SPAWN;
-			}
+	if (Foe.length) {
+		var closeFoe = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {
+			filter: (eCreep) => (!Memory.whiteList.includes(eCreep.owner.username))
 		});
-		if (spawnTarget) {
-			creep.moveTo(spawnTarget);
+		var foeDirection = creep.pos.getDirectionTo(closeFoe);
+		var y = 0;
+		var x = 0;
+		switch (foeDirection) {
+			case TOP:
+				y = -3;
+				break;
+			case TOP_RIGHT:
+				y = -3;
+				x = -3;
+				break;
+			case RIGHT:
+				x = -3;
+				break;
+			case BOTTOM_RIGHT:
+				y = 3;
+				x = -3;
+				break;
+			case BOTTOM:
+				y = 3;
+				break;
+			case BOTTOM_LEFT:
+				y = 3;
+				x = 3;
+				break;
+			case LEFT:
+				x = 3;
+				break;
+			case TOP_LEFT:
+				y = -3;
+				x = 3
+				break;
 		}
+		x = creep.pos.x + x;
+		y = creep.pos.y + y;
+		if (x < 0) {
+			x = 0;
+			if (y < 25 && y > 0) {
+				y = y - 1;
+			} else if (y < 49) {
+				y = y + 1;
+			}
+		} else if (x > 49) {
+			x = 49;
+			if (y < 25 && y > 0) {
+				y = y - 1;
+			} else if (y < 49) {
+				y = y + 1;
+			}
+		}
+		if (y < 0) {
+			y = 0;
+			if (x < 25 && x > 0) {
+				x = x - 1;
+			} else if (x < 49) {
+				x = x + 1;
+			}
+		} else if (y > 49) {
+			y = 49;
+			if (x < 25 && x > 0) {
+				x = x - 1;
+			} else if (x < 49) {
+				x = x + 1;
+			}
+		}
+
+		creep.moveTo(x, y, {
+			maxRooms: 1
+		});
 	}
 }
 
