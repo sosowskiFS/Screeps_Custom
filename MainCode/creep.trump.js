7e3894f002a2e87a6267a182ce0faae6bbf7bf9e
Montyblancs <montyblancs99@gmail.com>
2017-03-28 13:25:33 -0400

    egh

--- MainCode/creep.trump.js
+++ MainCode/creep.trump.js
@@ -61,4 +61,12 @@ var creep_trump = {
 	}
 };
 
+function repairCompare(a, b) {
+	if (a.hits < b.hits)
+		return -1;
+	if (a.hits > b.hits)
+		return 1;
+	return 0;
+}
+
 module.exports = creep_trump;
