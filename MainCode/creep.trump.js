83630bd0e3090cca4f7b1b6b35c20ae455f31da7
Montyblancs <montyblancs99@gmail.com>
2017-03-28 12:52:14 -0400

    Gotta make it stronger before greater

--- MainCode/creep.trump.js
+++ MainCode/creep.trump.js
@@ -21,32 +21,39 @@ var creep_trump = {
 				}
 			}
 		} else {
-			var targets = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
-			if (targets) {
-				if (creep.build(targets) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(targets);
+			if (creep.room.controller.level < 2) {
+				//Upgrade the controller
+				if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(creep.room.controller);
 				}
 			} else {
-				var closestDamagedStructure = [];
-				closestDamagedStructure = creep.room.find(FIND_STRUCTURES, {
-					filter: (structure) => (structure.structureType != STRUCTURE_ROAD) && (structure.hits < 100000)
-				});
-
-				if (closestDamagedStructure.length > 0) {
-					closestDamagedStructure.sort(repairCompare);
-					creep.memory.structureTarget = closestDamagedStructure[0].id;
-					if (creep.repair(closestDamagedStructure[0]) == ERR_NOT_IN_RANGE) {
-						creep.moveTo(closestDamagedStructure[0], {
-							reusePath: 25,
-							maxRooms: 1
-						});
+				var targets = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+				if (targets) {
+					if (creep.build(targets) == ERR_NOT_IN_RANGE) {
+						creep.moveTo(targets);
 					}
 				} else {
-					//America has been made great again.
-					if (Game.flags["WallThis"]) {
-						Game.flags["WallThis"].remove();
+					var closestDamagedStructure = [];
+					closestDamagedStructure = creep.room.find(FIND_STRUCTURES, {
+						filter: (structure) => (structure.structureType != STRUCTURE_ROAD) && (structure.hits < 100000)
+					});
+
+					if (closestDamagedStructure.length > 0) {
+						closestDamagedStructure.sort(repairCompare);
+						creep.memory.structureTarget = closestDamagedStructure[0].id;
+						if (creep.repair(closestDamagedStructure[0]) == ERR_NOT_IN_RANGE) {
+							creep.moveTo(closestDamagedStructure[0], {
+								reusePath: 25,
+								maxRooms: 1
+							});
+						}
 					} else {
-						creep.suicide();
+						//America has been made great again.
+						if (Game.flags["WallThis"]) {
+							Game.flags["WallThis"].remove();
+						} else {
+							creep.suicide();
+						}
 					}
 				}
 			}
