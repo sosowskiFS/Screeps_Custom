cb326d9f755257f98ed43604b7e6d92fc275549c
Montblanc <montyblancs99@gmail.com>
2017-04-02 10:03:14 -0400

    egh

--- MainCode/creep.asshealer.js
+++ MainCode/creep.asshealer.js
@@ -3,17 +3,15 @@ var creep_asshealer = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-        creep.heal(creep);
-
         var targetAttacker = _.filter(Game.creeps, (tCreep) => tCreep.name == creep.memory.attackerName);
         if (targetAttacker.length) {
             if (creep.pos.x == 0 || creep.pos.x == 49 || creep.pos.y == 0 || creep.pos.y == 49) {
                 var xTarget = 1;
                 var yTarget = 1;
-                if (creep.pos.x == 49){
+                if (creep.pos.x == 49) {
                     xTarget = 48;
                 }
-                if (creep.pos.y = 49){
+                if (creep.pos.y = 49) {
                     yTarget = 48;
                 }
                 creep.moveTo(xTarget, yTarget);
@@ -22,17 +20,25 @@ var creep_asshealer = {
             }
 
             if (targetAttacker[0].hits < targetAttacker[0].hitsMax) {
-                creep.rangedHeal(targetAttacker[0]);
-                creep.heal(targetAttacker[0]);
-                
+                if (creep.pos.getRangeTo(targetAttacker[0]) > 1) {
+                    creep.rangedHeal(targetAttacker[0]);
+                } else {
+                    creep.heal(targetAttacker[0]);
+                }
             } else {
-                var hurtAlly = creep.pos.findInRange(FIND_MY_CREEPS, 3, {
-                    filter: (thisCreep) => thisCreep.hits < thisCreep.hitsMax
-                });
-                if (hurtAlly.length > 0) {
-                    creep.rangedHeal(hurtAlly[0]);
-                    creep.heal(hurtAlly[0]);
-                    
+                if (creep.hits < creep.hitsMax) {
+                    creep.heal(creep);
+                } else {
+                    var hurtAlly = creep.pos.findInRange(FIND_MY_CREEPS, 3, {
+                        filter: (thisCreep) => thisCreep.hits < thisCreep.hitsMax
+                    });
+                    if (hurtAlly.length > 0) {
+                        if (creep.pos.getRangeTo(hurtAlly[0]) > 1) {
+                            creep.rangedHeal(hurtAlly[0]);
+                        } else {
+                            creep.heal(hurtAlly[0]);
+                        }
+                    }
                 }
             }
         } else {
