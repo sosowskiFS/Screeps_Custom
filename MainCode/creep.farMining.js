b22b397c34dddd172d88a0fd567fc39af2c19a29
Montyblancs <montyblancs99@gmail.com>
2017-04-05 12:43:30 -0400

    whoopsie

--- MainCode/creep.farMining.js
+++ MainCode/creep.farMining.js
@@ -560,39 +560,6 @@ var creep_farMining = {
 					if (creep.hits < creep.hitsMax) {
 						creep.heal(creep);
 					}
-				} else if (creep.room.controller && creep.room.controller.owner != "Montblanc") {
-					if (creep.hits < creep.hitsMax) {
-						creep.heal(creep);
-					}
-
-					//In target room
-					var eTowers = creep.room.find(FIND_HOSTILE_STRUCTURES, {
-						filter: (structure) => (structure.structureType == STRUCTURE_TOWER && structure.energy > 0)
-					});
-					if (eTowers.length) {
-						creep.moveTo(eTowers[0], {
-							ignoreDestructibleStructures: true
-						});
-						creep.attack(eTowers[0]);
-					} else {
-						var eSpawns = creep.room.find(FIND_HOSTILE_SPAWNS)
-						if (eSpawns.length) {
-							creep.moveTo(eSpawns[0], {
-								ignoreDestructibleStructures: true
-							});
-							creep.attack(eSpawns[0]);
-						} else {
-							var eStructures = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
-                                filter: (structure) => (structure.structureType != STRUCTURE_CONTROLLER)
-                            });
-                            if (eStructures) {
-                                creep.moveTo(eStructures, {
-                                    ignoreDestructibleStructures: true
-                                });
-                                creep.attack(eStructures);
-                            }
-						}
-					}
 				} else if (Game.flags[creep.memory.targetFlag]) {
 					if (creep.hits < creep.hitsMax) {
 						creep.heal(creep);
