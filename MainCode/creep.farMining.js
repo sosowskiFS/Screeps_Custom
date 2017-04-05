ad36ebf17c0a1534321eba77b7990b764665b7d5
Montyblancs <montyblancs99@gmail.com>
2017-04-05 16:05:38 -0400

    fix1

--- MainCode/creep.farMining.js
+++ MainCode/creep.farMining.js
@@ -185,9 +185,9 @@ var creep_farMining = {
 
 				if (creep.carry.energy > 0) {
 					//All creeps check for road under them and repair if needed.
-					var someSite = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
-					if (someSite) {
-						creep.build(someSite);
+					var someSite = creep.pos.findInRange(FIND_CONSTRUCTION_SITES, 3);
+					if (someSite.length) {
+						creep.build(someSite[0]);
 					} else {
 						var someStructure = creep.pos.lookFor(LOOK_STRUCTURES);
 						if (someStructure.length && (someStructure[0].hitsMax - someStructure[0].hits >= 100) && someStructure[0].structureType == STRUCTURE_ROAD) {
