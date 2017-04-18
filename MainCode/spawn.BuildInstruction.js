517e0f3c76ae762abc0ca2089a144c6ace833735
Montblanc <montyblancs99@gmail.com>
2017-04-18 07:02:05 -0400

    the novices want to play

--- MainCode/spawn.BuildInstruction.js
+++ MainCode/spawn.BuildInstruction.js
@@ -168,9 +168,9 @@ var spawn_BuildInstruction = {
 				var attackers = _.filter(Game.creeps, (creep) => creep.memory.priority == 'assattacker');
 				var healers = _.filter(Game.creeps, (creep) => creep.memory.priority == 'asshealer');
 				if (attackers.length < 1) {
-					//var attackerConfig = [TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, MOVE];
+					var attackerConfig = [TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, MOVE];
 					//Testing movement config
-					var attackerConfig = [TOUGH, TOUGH, MOVE, MOVE];
+					//var attackerConfig = [TOUGH, TOUGH, MOVE, MOVE];
 					if (spawn.canCreateCreep(attackerConfig) == OK) {
 						spawn.createCreep(attackerConfig, undefined, {
 							priority: 'assattacker',
@@ -183,9 +183,9 @@ var spawn_BuildInstruction = {
 						//console.log('Could not execute constructor. Spawn cannot create creep.');
 					}
 				} else if (healers.length < 2) {
-					//var healerConfig = [TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL];
+					var healerConfig = [TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL];
 					//Testing movement config
-					var healerConfig = [TOUGH, MOVE];
+					//var healerConfig = [TOUGH, MOVE];
 					var attackerName = '';
 					if (attackers[0]) {
 						attackerName = attackers[0].name
