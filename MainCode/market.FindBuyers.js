3ea5bc5ef8bcacf5b8ac69636bf98d95d9da4df2
Montyblancs <montyblancs99@gmail.com>
2017-05-03 09:13:01 -0400

    Attemps to sell all base minerals

--- MainCode/market.FindBuyers.js
+++ MainCode/market.FindBuyers.js
@@ -61,16 +61,7 @@ var market_buyers = {
 
 		if (TerminalEnergy >= 50000) {
 			var currentMineral = Game.getObjectById(thisMineral);
-			var MaxSaleAmount = 30000;
-			var mineralInTerminal = thisTerminal.store[currentMineral.mineralType] - 20000;
 
-			if (mineralInTerminal > MaxSaleAmount) {
-				mineralInTerminal = MaxSaleAmount;
-			}
-			if (!Memory.PriceList[currentMineral.mineralType]) {
-				//Initalize this memory object
-				Memory.PriceList[currentMineral.mineralType] = 0;
-			}
 
 			//Memory.mineralNeed
 
@@ -88,22 +79,37 @@ var market_buyers = {
 				}
 			}
 
-			if (mineralInTerminal > 100 && TerminalEnergy >= 100000 && Game.time % 1000 == 0) {
-				var FilteredOrders = Game.market.getAllOrders(order => order.resourceType == currentMineral.mineralType && order.type == ORDER_BUY && order.price >= Memory.PriceList[currentMineral.mineralType] && Game.market.calcTransactionCost(mineralInTerminal, thisRoom.name, order.roomName) <= TerminalEnergy)
-				if (FilteredOrders.length > 0) {
-					FilteredOrders.sort(orderPriceCompare);
-					var tradeAmount = FilteredOrders[0].amount;
-					if (mineralInTerminal < tradeAmount) {
-						tradeAmount = mineralInTerminal;
-					}
-					if (Game.market.deal(FilteredOrders[0].id, tradeAmount, thisRoom.name) == OK) {
-						console.log('Successfully made a deal');
-						Memory.PriceList[currentMineral.mineralType] = FilteredOrders[0].price;
-					}
-				} else {
-					//No orders were found with mineral in the terminal, with MAX ENERGY in the terminal. Drop the price a bit
-					if (Memory.PriceList[currentMineral.mineralType] > 0) {
-						Memory.PriceList[currentMineral.mineralType] = Memory.PriceList[currentMineral.mineralType] - 0.01;
+			var sellMinerals = [RESOURCE_HYDROGEN, RESOURCE_OXYGEN, RESOURCE_UTRIUM, RESOURCE_LEMERGIUM, RESOURCE_KEANIUM, RESOURCE_ZYNTHIUM, RESOURCE_CATALYST];
+
+			if (TerminalEnergy >= 100000 && Game.time % 1000 == 0) {
+				var MaxSaleAmount = 30000;
+				for (var y in sellMinerals) {
+					var mineralInTerminal = thisTerminal.store[sellMinerals[y]] - 20000;
+					if (mineralInTerminal > 0) {
+						if (mineralInTerminal > MaxSaleAmount) {
+							mineralInTerminal = MaxSaleAmount;
+						}
+						if (!Memory.PriceList[sellMinerals[y]]) {
+							//Initalize this memory object
+							Memory.PriceList[sellMinerals[y]] = 0;
+						}
+						var FilteredOrders = Game.market.getAllOrders(order => order.resourceType == currentMineral.mineralType && order.type == ORDER_BUY && order.price >= Memory.PriceList[sellMinerals[y]] && Game.market.calcTransactionCost(mineralInTerminal, thisRoom.name, order.roomName) <= TerminalEnergy)
+						if (FilteredOrders.length > 0) {
+							FilteredOrders.sort(orderPriceCompare);
+							var tradeAmount = FilteredOrders[0].amount;
+							if (mineralInTerminal < tradeAmount) {
+								tradeAmount = mineralInTerminal;
+							}
+							if (Game.market.deal(FilteredOrders[0].id, tradeAmount, thisRoom.name) == OK) {
+								console.log('Successfully made a deal');
+								Memory.PriceList[sellMinerals[y]] = FilteredOrders[0].price;
+							}
+						} else {
+							//No orders were found with mineral in the terminal, with MAX ENERGY in the terminal. Drop the price a bit
+							if (Memory.PriceList[currentMineral.mineralType] > 0) {
+								Memory.PriceList[currentMineral.mineralType] = Memory.PriceList[sellMinerals[y]] - 0.01;
+							}
+						}
 					}
 				}
 			}
