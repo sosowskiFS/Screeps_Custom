1db4cb9c13d279e50995e39cda0779d8b2de6e14
Montyblancs <montyblancs99@gmail.com>
2017-06-01 09:38:09 -0400

    Don't ignore creeps anymore

--- MainCode/creep.work5.js
+++ MainCode/creep.work5.js
@@ -482,9 +482,7 @@ var creep_work5 = {
                 }
 
                 if (Game.flags[creep.room.name + "Supply"] && (creep.pos.x != Game.flags[creep.room.name + "Supply"].pos.x || creep.pos.y != Game.flags[creep.room.name + "Supply"].pos.y)) {
-                    creep.moveTo(Game.flags[creep.room.name + "Supply"], {
-                        ignoreCreeps: true
-                    });
+                    creep.moveTo(Game.flags[creep.room.name + "Supply"]);
                 } else if (_.sum(creep.carry) == 0) {
                     //Get from storage
                     var storageTarget = creep.room.storage;
