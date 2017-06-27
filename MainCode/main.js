ca74be382aaced4a196bc4dd14454f5ff1143c76
Montyblancs <montyblancs99@gmail.com>
2017-06-27 08:45:07 -0400

    One flag oblitterated an entire room

--- MainCode/main.js
+++ MainCode/main.js
@@ -306,7 +306,7 @@ module.exports.loop = function() {
                 //Get non-suppliers off the supplier spot
                 if (Game.flags[thisRoom.name + "Supply"]) {
                     var creepCheck = Game.flags[thisRoom.name + "Supply"].pos.lookFor(LOOK_CREEPS);
-                    if (creepCheck.length && creepCheck[0].owner.username == "Montblanc" && creepCheck[0].memory.priority != "supplier") {
+                    if (thisRoom.controller.level >= 6 && creepCheck.length && creepCheck[0].owner.username == "Montblanc" && creepCheck[0].memory.priority != "supplier") {
                         //Get the fuck off!
                         creepCheck[0].suicide();
                     }
