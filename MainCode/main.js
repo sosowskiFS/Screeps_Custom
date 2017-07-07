67ec6bf8de09fe5d469d61bf6c11566a3a28fa0b
Montyblancs <montyblancs99@gmail.com>
2017-07-07 10:01:43 -0400

    Defender generation testing

--- MainCode/main.js
+++ MainCode/main.js
@@ -684,7 +684,7 @@ module.exports.loop = function() {
                         Memory.roomsUnderAttack.push(towers[y].room.name);
                         RampartDirection = "Closed";
                         if (hostiles[0].owner.username == 'Invader' || (hostiles[0].hitsMax <= 100 && hostiles.length == 1)) {
-                            Memory.roomsPrepSalvager.push(towers[y].room.name);
+                            //Memory.roomsPrepSalvager.push(towers[y].room.name);
                         } else if (Memory.RoomsAt5.indexOf(towers[y].room.name) == -1 && (hostiles[0].hits > 100 || hostiles.length > 1)) {
                             //No good combat code! SAFE MODE!
                             if (!towers[y].room.controller.safeMode && (hostiles[0].getActiveBodyparts(ATTACK) > 0 || hostiles[0].getActiveBodyparts(RANGED_ATTACK) > 0 || hostiles[0].getActiveBodyparts(WORK) > 0)) {
