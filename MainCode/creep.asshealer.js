9ff7b28232ed81000eccfffe11ce1c0b11fd1662
Montyblancs <montyblancs99@gmail.com>
2017-07-08 14:43:16 -0400

    what

--- MainCode/creep.asshealer.js
+++ MainCode/creep.asshealer.js
@@ -58,7 +58,7 @@ var creep_asshealer = {
                         filter: (structure) => (structure.structureType == STRUCTURE_PORTAL)
                     });
                 }
-                if (thisPortal) {
+                if (thisPortal && targetAttacker.pos.roomName != creep.pos.roomName) {
                     creep.travelTo(thisPortal);
                 } else if ((creep.pos.x == 0 || creep.pos.x == 49 || creep.pos.y == 0 || creep.pos.y == 49) && targetAttacker.room.name == creep.room.name) {
                     var xTarget = 0;
@@ -126,7 +126,7 @@ var creep_asshealer = {
             } else {
                 creep.heal(creep);
                 if (Game.flags["RallyHere"] && Game.flags["RallyHere"].pos) {
-                    creep.travelTo(Game.flags["RallyHere"], {
+                    creep.moveTo(Game.flags["RallyHere"], {
                         ignoreRoads: true
                     });
                 }
