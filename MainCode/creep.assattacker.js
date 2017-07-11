dee61b2f43d2d398eaf8dec9e85e51bcce506c04
Montyblancs <montyblancs99@gmail.com>
2017-07-11 11:50:16 -0400

    Temp whitelist

--- MainCode/creep.assattacker.js
+++ MainCode/creep.assattacker.js
@@ -31,7 +31,7 @@ var creep_assattacker = {
         }
 
         var closeFoe = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {
-            filter: (eCreep) => (!Memory.whiteList.includes(eCreep.owner.username))
+            filter: (eCreep) => (!Memory.whiteList.includes(eCreep.owner.username) && eCreep.owner.username != 'Eiskalt')
         });
 
         var thisHealer = Game.getObjectById(creep.memory.healerID);
