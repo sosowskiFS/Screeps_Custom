afbc8dee0077378a87b2cdd58aa9dca9fd5b0231
Montyblancs <montyblancs99@gmail.com>
2017-07-13 21:02:20 -0400

    MISSED ONE

--- MainCode/creep.work5.js
+++ MainCode/creep.work5.js
@@ -112,13 +112,13 @@ var creep_work5 = {
                     }
                 }
 
-                if (!creep.memory.hasBoosted && creep.room.controller.level >= 7 && creep.room.controller.level != 8 && Memory.labList[creep.room.name].length >= 6 && !creep.memory.previousPriority) {
+                if (!creep.memory.hasBoosted && creep.room.controller.level >= 7 && Memory.labList[creep.room.name].length >= 6 && !creep.memory.previousPriority) {
                     var mineralCost = creep.getActiveBodyparts(WORK) * LAB_BOOST_MINERAL;
                     var energyCost = creep.getActiveBodyparts(WORK) * LAB_BOOST_ENERGY;
                     var upgradeLab = creep.room.find(FIND_MY_STRUCTURES, {
                         filter: (structure) => (structure.structureType == STRUCTURE_LAB && structure.mineralType == RESOURCE_CATALYZED_GHODIUM_ACID)
                     });
-                    if (upgradeLab.length && upgradeLab[0].mineralAmount >= mineralCost && upgradeLab.energy >= energyCost) {
+                    if (upgradeLab.length && upgradeLab[0].mineralAmount >= mineralCost && upgradeLab[0].energy >= energyCost) {
                         creep.travelTo(upgradeLab[0]);
                         if (upgradeLab[0].boostCreep(creep) == OK) {
                             creep.memory.hasBoosted = true;
