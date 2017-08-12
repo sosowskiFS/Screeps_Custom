82a01052cd74ae984e0638d3c8046f48511a1b77
Montyblancs <montyblancs99@gmail.com>
2017-08-12 07:44:06 -0400

    Don't run room creeps if using too much cpu!

--- MainCode/main.js
+++ MainCode/main.js
@@ -930,7 +930,7 @@ module.exports.loop = function() {
                     default:
                         if (Memory.RoomsAt5.indexOf(creep.room.name) === -1) {
                             var pre = Game.cpu.getUsed();
-                            if (pre <= Game.cpu.tickLimit - 50) {
+                            if (pre <= Game.cpu.limit) {
                                 creep_work.run(creep, 25);
                                 pre5CPU = pre5CPU + (Game.cpu.getUsed() - pre);
                             }
@@ -940,7 +940,7 @@ module.exports.loop = function() {
                                 creep_work.run(creep, 25);
                             } else {
                                 var pre = Game.cpu.getUsed();
-                                if (pre <= Game.cpu.tickLimit - 50) {
+                                if (pre <= Game.cpu.limit) {
                                     creep_work5.run(creep);
                                     post5CPU = post5CPU + (Game.cpu.getUsed() - pre);
                                 }
