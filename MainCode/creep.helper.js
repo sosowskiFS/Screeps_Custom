01a9da5e9addf4b8ce69894390662badffd2424e
Montyblancs <montyblancs99@gmail.com>
2017-08-23 12:22:11 -0400

    Temp bypass

--- MainCode/creep.helper.js
+++ MainCode/creep.helper.js
@@ -16,7 +16,8 @@ var creep_Helper = {
                 if (creep.memory.path[0] == creep.room.name) {
                     creep.memory.path.splice(0, 1);
                 }
-                creep.travelTo(new RoomPosition(25, 25, creep.memory.path[0]));
+                //creep.travelTo(new RoomPosition(25, 25, creep.memory.path[0]));
+                creep.travelTo(new RoomPosition(25, 25, creep.memory.destination));
             } else {
                 creep.travelTo(new RoomPosition(25, 25, creep.memory.destination));
             }
