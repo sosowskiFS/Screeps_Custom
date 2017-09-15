752eade775dc4e530a6e301e2e8e25403fd73152
Montyblancs <montyblancs99@gmail.com>
2017-09-15 10:00:03 -0400

    Slightly lower flag requirements

--- MainCode/main.js
+++ MainCode/main.js
@@ -577,7 +577,7 @@ module.exports.loop = function() {
                     //Search observed room for power bank
                     if (Game.rooms[Memory.powerCheckList[thisRoom.name][0]]) {
                         var powerbanks = Game.rooms[Memory.powerCheckList[thisRoom.name][0]].find(FIND_STRUCTURES, {
-                            filter: (eStruct) => (eStruct.structureType == STRUCTURE_POWER_BANK && eStruct.power >= 3000 && eStruct.ticksToDecay >= 4500)
+                            filter: (eStruct) => (eStruct.structureType == STRUCTURE_POWER_BANK && eStruct.power >= 1650 && eStruct.ticksToDecay >= 4500)
                         });
                         if (powerbanks.length) {
                             Game.rooms[Memory.powerCheckList[thisRoom.name][0]].createFlag(powerbanks[0].pos.x, powerbanks[0].pos.y, thisRoom.name + "PowerGather");
