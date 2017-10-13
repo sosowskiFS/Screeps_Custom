c00cb06a6467b83cd9c8fe8f3697a39b24f5b23a
Montyblancs <montyblancs99@gmail.com>
2017-10-13 10:49:24 -0400

    NEAT O.

--- MainCode/creep.looter.js
+++ MainCode/creep.looter.js
@@ -2,20 +2,20 @@ var creep_looter = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if (creep.room.name != creep.memory.destination && _.sum(creep.carry) <= 500) {
+        if (creep.room.name != creep.memory.destination && _.sum(creep.carry) <= creep.carryCapacity - 100) {
             if (Game.rooms[creep.memory.destination] && Game.rooms[creep.memory.destination].storage) {
                 creep.travelTo(Game.rooms[creep.memory.destination].storage);
             } else {
                 creep.travelTo(new RoomPosition(25, 25, creep.memory.destination));
             }
-        } else if (creep.room.name != creep.memory.homeRoom && _.sum(creep.carry) > 500) {
+        } else if (creep.room.name != creep.memory.homeRoom && _.sum(creep.carry) > creep.carryCapacity - 100) {
             if (Game.rooms[creep.memory.homeRoom] && Game.rooms[creep.memory.homeRoom].storage) {
                 creep.travelTo(Game.rooms[creep.memory.homeRoom].storage);
             } else {
                 creep.travelTo(new RoomPosition(25, 25, creep.memory.homeRoom));
             }
         } else {
-            if (_.sum(creep.carry) >= creep.carryCapacity - 300) {
+            if (_.sum(creep.carry) > creep.carryCapacity - 100) {
                 //In far room, loot container
                 if (creep.room.storage) {
                     if (_.sum(creep.room.storage.store) <= 0) {
