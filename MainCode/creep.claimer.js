ef8f836c3a4aec9ae792c03b31b3a9d9a577b518
Montyblancs <sosowski@comcast.net>
2018-01-18 13:20:06 -0500

    OFFROOOOOOAD

--- MainCode/creep.claimer.js
+++ MainCode/creep.claimer.js
@@ -23,30 +23,35 @@ var creep_claimer = {
                 }
 
                 creep.travelTo(thisPortal, {
-                    ignoreRoads: true
+                    ignoreRoads: true,
+                    offRoad: true
                 });
             } else if (creep.memory.path && creep.memory.path.length) {
                 if (creep.memory.path[0] == creep.room.name) {
                     creep.memory.path.splice(0, 1);
                 }
                 creep.travelTo(new RoomPosition(25, 25, creep.memory.path[0]), {
-                    ignoreRoads: true
+                    ignoreRoads: true,
+                    offRoad: true
                 });
             } else {
                 if (Game.flags["ClaimThis"] && Game.flags["ClaimThis"].pos) {
                     creep.travelTo(Game.flags["ClaimThis"], {
-                        ignoreRoads: true
+                        ignoreRoads: true,
+                        offRoad: true
                     });
                 } else {
                     creep.travelTo(new RoomPosition(25, 25, creep.memory.destination), {
-                        ignoreRoads: true
+                        ignoreRoads: true,
+                        offRoad: true
                     });
                 }
             }
         } else {
             if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.travelTo(creep.room.controller, {
-                    ignoreRoads: true
+                    ignoreRoads: true,
+                    offRoad: true
                 });
             } else if (creep.claimController(creep.room.controller) == OK) {
                 Memory.claimSpawn = false;
