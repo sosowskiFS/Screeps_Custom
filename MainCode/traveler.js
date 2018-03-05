409e5480551968831ed5cd59789613ea8bdd94aa
Montyblancs <sosowski@comcast.net>
2018-03-05 15:37:04 -0500

    note for traveler overwrite

--- MainCode/traveler.js
+++ MainCode/traveler.js
@@ -581,4 +581,5 @@ const STATE_DEST_ROOMNAME = 6;
 // assigns a function to Creep.prototype: creep.travelTo(destination)
 Creep.prototype.travelTo = function (destination, options) {
     return Traveler.travelTo(this, destination, options);
+   //return this.moveTo(destination, options);
 };
