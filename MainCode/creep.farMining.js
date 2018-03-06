2d9bcecf2a906b75edab39dd777758d9093ce6e8
Montyblancs <sosowski@comcast.net>
2018-03-06 13:11:56 -0500

    Increase tombstone search range

--- MainCode/creep.farMining.js
+++ MainCode/creep.farMining.js
@@ -545,7 +545,7 @@ var creep_farMining = {
 
                     if (_.sum(creep.carry) < creep.carryCapacity - 100) {
                         if (Game.flags[creep.room.name + "SKRoom"]) {
-                            var sources = creep.pos.findInRange(FIND_TOMBSTONES, 3);
+                            var sources = creep.pos.findInRange(FIND_TOMBSTONES, 5);
                             if (sources.length && _.sum(sources[0].store) > 0 && Object.keys(sources[0].store).length > 1) {
                                 if (creep.withdraw(sources[0], Object.keys(sources[0].store)[1]) == ERR_NOT_IN_RANGE) {
                                     creep.travelTo(sources[0], {
