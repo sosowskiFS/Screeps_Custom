63d4bc593b9e69f966d5c92e83777d5d12b58a6d
Montyblancs <montyblancs99@gmail.com>
2018-07-17 10:20:06 -0400

    Whoops

--- MainCode/creep.assattacker.js
+++ MainCode/creep.assattacker.js
@@ -178,7 +178,7 @@ var creep_assattacker = {
                         }
                     }
 
-                    creep.rangedMassAttack();
+                    //creep.rangedMassAttack();
                     if (creep.hits < creep.hitsMax - 1700 && Game.flags[creep.memory.homeRoom + "FallBack"] && Game.flags[creep.memory.homeRoom + "FallBack"].pos) {
                         //Fall back
                         creep.travelTo(Game.flags[creep.memory.homeRoom + "FallBack"], {
