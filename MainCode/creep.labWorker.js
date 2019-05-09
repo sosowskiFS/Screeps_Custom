4203da5a0a23c4a68946a7d22d389deeee991578
Montyblancs <montyblancs99@gmail.com>
2019-05-09 12:40:59 -0400

    Apply anti-scalp to new orders too

--- MainCode/creep.labWorker.js
+++ MainCode/creep.labWorker.js
@@ -76,7 +76,11 @@ var creep_labWorker = {
                                     var targetPrice = comparableOrders[0].price;
                                     if (Memory.RoomsAt5.indexOf(comparableOrders[0].roomName) == -1) {
                                         //Not competing with self, undercut!
-                                        targetPrice = targetPrice - 0.001
+                                        if (targetPrice < 1) {
+                                            targetPrice = 1
+                                        } else {
+                                            targetPrice = targetPrice - 0.001
+                                        } 
                                     }
                                     Game.market.createOrder(ORDER_SELL, creep.memory.mineral6, targetPrice, creep.room.terminal.store[creep.memory.mineral6], creep.room.name);
                                 }
