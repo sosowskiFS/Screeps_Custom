c5a2b229e98f0e5a2aabb47e52f217812ad91e7b
Montyblancs <montyblancs99@gmail.com>
2020-06-21 18:44:12 -0400

    Missed a room

--- MainCode/main.js
+++ MainCode/main.js
@@ -1828,7 +1828,7 @@ function memCheck() {
     Memory.powerCheckList["E57N39"] = ["E56N40", "E57N40", "E58N40"];
     Memory.powerCheckList["E51N23"] = ["E50N24", "E50N23", "E50N22"];
     Memory.powerCheckList["E42N31"] = ["E42N30", "E43N30", "E44N30"];
-    Memory.powerCheckList["W2N11"] = ["W4N10", "W3N10", "W2N10", "W1N10"];
+    Memory.powerCheckList["W2N11"] = ["W4N10", "W3N10", "W2N10", "W1N10", "W0N10"];
 
     if (!Memory.observerList) {
         Memory.observerList = new Object();
